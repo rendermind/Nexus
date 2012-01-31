5c3e55dba1719b0b6c76d47a5e19ec0a95f7c679
Stephen Monroe <stephen.monroe@me.com>
2012-01-30 22:08:00 -0600

    updated to 1.1-r3

--- src/main/java/com/vioviocity/nexus/NexusPlayerListener.java
+++ src/main/java/com/vioviocity/nexus/NexusPlayerListener.java
@@ -1,7 +1,6 @@
 package com.vioviocity.nexus;
 
 import java.text.DecimalFormat;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
